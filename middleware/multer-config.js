e87f357d86f848d16b0d2b213ed5ddffc178ff9b
DEBUIRE Alexis <alexisdebuire@gmail.com>
2024-03-26 09:29:22 +0100

    Adding sharp to convert to WebP

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -13,8 +13,19 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_");
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
+    callback(null, name + Date.now() + ".webp");
   },
 });
 
-module.exports = multer({ storage: storage }).single("image");
+module.exports = { storage };
+
+/*
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  },
+*/
--- routes/book.js
+++ routes/book.js
@@ -2,18 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/auth");
-const multer = require("../middleware/multer-config");
+const sharp = require("../middleware/sharp-config");
 
 const bookCtrl = require("../controllers/book");
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/bestrating", bookCtrl.getBestratingBooks);
 router.get("/:id", bookCtrl.getOneBook);
 
-router.post("/", auth, multer, bookCtrl.createBook);
+router.post("/", auth, upload.single("image"), sharp, bookCtrl.createBook);
 router.post("/:id/rating", auth, bookCtrl.createRatingBook);
 
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.put("/:id", auth, upload.single("image"), sharp, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
